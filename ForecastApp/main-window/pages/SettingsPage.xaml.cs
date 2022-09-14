432530f827ed0bc5e5b62daa3f0849c4fa52bd50
Александр Баскаков <basc11111@gmail.com>
2022-09-14 17:39:03 +0300

    Design ProductSettingsPage.xaml

--- ForecastApp/main-window/pages/SettingsPage.xaml.cs
+++ ForecastApp/main-window/pages/SettingsPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using ForecastApp.main_window.pages.settings_page;
 
 namespace ForecastApp.main_window.pages;
 
 public partial class SettingsPage : UserControl{
+    
     private static readonly CategorySettingsPage _categorySettingsPage = new CategorySettingsPage();
+    private static readonly ProductSettingsPage _productSettingsPage = new ProductSettingsPage();
     public SettingsPage(){
         InitializeComponent();
     }
@@ -12,5 +15,10 @@ public partial class SettingsPage : UserControl{
     private void CategorySettingsPageBtn_OnClick(object sender, RoutedEventArgs e){
         RenderSubPage.Children.Clear();
         RenderSubPage.Children.Add(_categorySettingsPage);
+    }
+
+    private void ProductSettingsPageBtn_OnClick(object sender, RoutedEventArgs e){
+        RenderSubPage.Children.Clear();
+        RenderSubPage.Children.Add(_productSettingsPage);
     }
 }
