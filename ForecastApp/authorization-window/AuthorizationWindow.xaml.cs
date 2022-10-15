83d9c0616c05262efcdccf0f2ae80fdcf674f44f
Александр Баскаков <basc11111@gmail.com>
2022-10-15 16:52:26 +0300

    create AuthorizationWindow.xaml

---
+++ ForecastApp/authorization-window/AuthorizationWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ForecastApp.authorization_window;
+
+public partial class AuthorizationWindow : Window{
+    public AuthorizationWindow(){
+        InitializeComponent();
+    }
+}
