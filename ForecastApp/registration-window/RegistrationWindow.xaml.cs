2cd8f050c35907b1b0a6dc06ad0f78ac8a424b9a
Александр Баскаков <basc11111@gmail.com>
2022-10-15 17:08:48 +0300

    create RegistrationWindow.xaml

---
+++ ForecastApp/registration-window/RegistrationWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ForecastApp.registration_window;
+
+public partial class RegistrationWindow : Window{
+    public RegistrationWindow(){
+        InitializeComponent();
+    }
+}
